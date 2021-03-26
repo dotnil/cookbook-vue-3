7bee7f4fee9c6baa0b28f9b261d1e9c4577965f2
Tatiana Kh <ts.khol@gmail.com>
2021-03-26 21:41:59 +0300

    add ObjectSans font

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      The quick brown fox jumps over the lazy dog. — Эх, чужак, общий съём цен шляп (юфть) — вдрызг!
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    </p>
+  </div>
+</template>
